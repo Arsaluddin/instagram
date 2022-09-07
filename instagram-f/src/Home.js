fc2f2ec9d3ae2782e3116a4a4099d66349afac84
arsal uddin <arsaluddin134@gmail.com>
2022-09-07 20:11:38 +0530

    create components

---
+++ instagram-f/src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+import  Story from'./Story.js';
+import Posts from './Post.js';
+
+const Home = () => {
+
+    return(
+        <>
+           <Story/>
+           <Posts/>
+        </>
+    )
+}
+
+export default Home;
---
+++ instagram-f/src/Story.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Story.css';
+
+
+const Story = () => {
+
+    return(
+        <>
+            <div className="StoryHeader">
+               <div className="Story">
+                    <h1>arsal</h1>
+              </div>       
+            </div>
+        </>
+    )
+}
+
+export default Story;
+
